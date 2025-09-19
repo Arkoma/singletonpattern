69ef7ffde04f772096c5475ab36fee6256f520eb
Aaron Burk <aburk@veracode.com>
2025-09-19 16:17:03 -0400

    state being managed by this class is unmanageable because the ChocolateBoiler class is not a singleton

---
+++ src/main/java/com/singletonPattern/ChocolateBoiler.java
@@ -0,0 +1,39 @@
+package com.singletonPattern;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+            // fill the boiler with milk/chocolate mixture
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            // bring Contents to a boil
+            boiled = true;
+        }
+    }
+
+    private boolean isBoiled() {
+        return boiled;
+    }
+
+    private boolean isEmpty() {
+        return empty;
+    }
+}
